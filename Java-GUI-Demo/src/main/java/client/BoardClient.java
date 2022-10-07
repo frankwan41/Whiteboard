cbd3244429d5e38c1ad5bf152f7a63ba39529a19
frankwan41 <yicongw@student.unimelb.edu.au>
2022-10-08 01:12:32 +1100

    Implement icon and panels location

--- Java-GUI-Demo/src/main/java/client/BoardClient.java
+++ Java-GUI-Demo/src/main/java/client/BoardClient.java
@@ -31,11 +31,17 @@ public class BoardClient extends javax.swing.JFrame {
     private void initComponents() {
 
         buttonGroup1 = new javax.swing.ButtonGroup();
-        drawPanel = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        userList = new javax.swing.JLabel();
-        jScrollPane4 = new javax.swing.JScrollPane();
-        jScrollPane5 = new javax.swing.JScrollPane();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        userListPanel = new javax.swing.JScrollPane();
+        userList = new javax.swing.JList<>();
+        chatBoxPanel = new javax.swing.JScrollPane();
+        chatList = new javax.swing.JList<>();
+        boardPanel = new javax.swing.JPanel();
+        drawLabel = new javax.swing.JLabel();
+        inputPanel = new javax.swing.JScrollPane();
+        inputArea = new javax.swing.JTextArea();
+        userListLabel = new javax.swing.JLabel();
+        chatLabel = new javax.swing.JLabel();
         menuBar = new javax.swing.JMenuBar();
         fileMenu = new javax.swing.JMenu();
         newBoard = new javax.swing.JMenuItem();
@@ -43,33 +49,70 @@ public class BoardClient extends javax.swing.JFrame {
         fileSaveAs = new javax.swing.JMenuItem();
         fileClose = new javax.swing.JMenuItem();
         shapeMenu = new javax.swing.JMenu();
-        drawRect = new javax.swing.JRadioButtonMenuItem();
         drawLine = new javax.swing.JRadioButtonMenuItem();
+        drawRect = new javax.swing.JRadioButtonMenuItem();
         drawTri = new javax.swing.JRadioButtonMenuItem();
         drawCir = new javax.swing.JRadioButtonMenuItem();
         colorMenu = new javax.swing.JMenu();
         textMenu = new javax.swing.JMenu();
+        drawingMenu = new javax.swing.JMenu();
+        cursorMenu = new javax.swing.JMenu();
+        currentTool = new javax.swing.JMenu();
+
+        jMenuItem1.setText("jMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout drawPanelLayout = new javax.swing.GroupLayout(drawPanel);
-        drawPanel.setLayout(drawPanelLayout);
-        drawPanelLayout.setHorizontalGroup(
-            drawPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 335, Short.MAX_VALUE)
+        userList.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        userListPanel.setViewportView(userList);
+
+        chatList.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        chatBoxPanel.setViewportView(chatList);
+
+        drawLabel.setText("Drawing Section");
+
+        javax.swing.GroupLayout boardPanelLayout = new javax.swing.GroupLayout(boardPanel);
+        boardPanel.setLayout(boardPanelLayout);
+        boardPanelLayout.setHorizontalGroup(
+            boardPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(boardPanelLayout.createSequentialGroup()
+                .addGap(207, 207, 207)
+                .addComponent(drawLabel)
+                .addContainerGap(243, Short.MAX_VALUE))
         );
-        drawPanelLayout.setVerticalGroup(
-            drawPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+        boardPanelLayout.setVerticalGroup(
+            boardPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(boardPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(drawLabel)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        userList.setText("Users List");
-        userList.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        jScrollPane1.setViewportView(userList);
+        inputArea.setColumns(20);
+        inputArea.setRows(5);
+        inputPanel.setViewportView(inputArea);
 
+        userListLabel.setText("Participants");
+
+        chatLabel.setText("Chat");
+
+        menuBar.setMaximumSize(new java.awt.Dimension(200, 30));
+        menuBar.setMinimumSize(new java.awt.Dimension(20, 20));
+        menuBar.setPreferredSize(new java.awt.Dimension(300, 27));
+
+        fileMenu.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/folder.png")); // NOI18N
         fileMenu.setText("File");
 
         newBoard.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_N, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        newBoard.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/newboard.png")); // NOI18N
         newBoard.setText("New Board");
         newBoard.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -79,6 +122,7 @@ public class BoardClient extends javax.swing.JFrame {
         fileMenu.add(newBoard);
 
         fileSave.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        fileSave.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/save.png")); // NOI18N
         fileSave.setText("Save");
         fileSave.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -88,6 +132,7 @@ public class BoardClient extends javax.swing.JFrame {
         fileMenu.add(fileSave);
 
         fileSaveAs.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_D, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        fileSaveAs.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/saveas.png")); // NOI18N
         fileSaveAs.setText("Save As");
         fileSaveAs.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -97,6 +142,7 @@ public class BoardClient extends javax.swing.JFrame {
         fileMenu.add(fileSaveAs);
 
         fileClose.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        fileClose.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/close.png")); // NOI18N
         fileClose.setText("Close Board");
         fileClose.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -107,6 +153,7 @@ public class BoardClient extends javax.swing.JFrame {
 
         menuBar.add(fileMenu);
 
+        shapeMenu.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/shape.png")); // NOI18N
         shapeMenu.setText("Shape");
         shapeMenu.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -114,12 +161,10 @@ public class BoardClient extends javax.swing.JFrame {
             }
         });
 
-        buttonGroup1.add(drawRect);
-        drawRect.setText("Rectangle");
-        shapeMenu.add(drawRect);
-
+        drawLine.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_L, java.awt.event.InputEvent.CTRL_DOWN_MASK));
         buttonGroup1.add(drawLine);
         drawLine.setText("Line");
+        drawLine.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/line.png")); // NOI18N
         drawLine.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 drawLineActionPerformed(evt);
@@ -127,12 +172,22 @@ public class BoardClient extends javax.swing.JFrame {
         });
         shapeMenu.add(drawLine);
 
+        drawRect.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_R, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        buttonGroup1.add(drawRect);
+        drawRect.setText("Rectangle");
+        drawRect.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/rectangle.png")); // NOI18N
+        shapeMenu.add(drawRect);
+
+        drawTri.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_T, java.awt.event.InputEvent.CTRL_DOWN_MASK));
         buttonGroup1.add(drawTri);
         drawTri.setText("Triangle");
+        drawTri.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/triangle.png")); // NOI18N
         shapeMenu.add(drawTri);
 
+        drawCir.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.CTRL_DOWN_MASK));
         buttonGroup1.add(drawCir);
         drawCir.setText("Circle");
+        drawCir.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/circle.png")); // NOI18N
         drawCir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 drawCirActionPerformed(evt);
@@ -142,11 +197,22 @@ public class BoardClient extends javax.swing.JFrame {
 
         menuBar.add(shapeMenu);
 
+        colorMenu.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/color.png")); // NOI18N
         colorMenu.setText("Color");
         menuBar.add(colorMenu);
 
+        textMenu.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/text.png")); // NOI18N
         textMenu.setText("Text");
         menuBar.add(textMenu);
+
+        drawingMenu.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/freedraw.png")); // NOI18N
+        menuBar.add(drawingMenu);
+
+        cursorMenu.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/cursor.png")); // NOI18N
+        menuBar.add(cursorMenu);
+
+        currentTool.setIcon(new javax.swing.ImageIcon("/Users/ycw/Desktop/distributed system/Project2/Whiteboard/Java-GUI-Demo/src/main/java/icon/cursor.png")); // NOI18N
+        menuBar.add(currentTool);
 
         setJMenuBar(menuBar);
 
@@ -155,23 +221,42 @@ public class BoardClient extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(drawPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap()
+                .addComponent(boardPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jScrollPane4, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(userListPanel, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(chatBoxPanel, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(inputPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 45, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(chatLabel)
+                                .addGap(107, 107, 107))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(userListLabel)
+                                .addGap(88, 88, 88))))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(drawPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane5, javax.swing.GroupLayout.DEFAULT_SIZE, 75, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(userListLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(userListPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(chatLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(chatBoxPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(inputPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 6, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(boardPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap())))
         );
 
         pack();
@@ -244,24 +329,33 @@ public class BoardClient extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel boardPanel;
     private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JScrollPane chatBoxPanel;
+    private javax.swing.JLabel chatLabel;
+    private javax.swing.JList<String> chatList;
     private javax.swing.JMenu colorMenu;
+    private javax.swing.JMenu currentTool;
+    private javax.swing.JMenu cursorMenu;
     private javax.swing.JRadioButtonMenuItem drawCir;
+    private javax.swing.JLabel drawLabel;
     private javax.swing.JRadioButtonMenuItem drawLine;
-    private javax.swing.JPanel drawPanel;
     private javax.swing.JRadioButtonMenuItem drawRect;
     private javax.swing.JRadioButtonMenuItem drawTri;
+    private javax.swing.JMenu drawingMenu;
     private javax.swing.JMenuItem fileClose;
     private javax.swing.JMenu fileMenu;
     private javax.swing.JMenuItem fileSave;
     private javax.swing.JMenuItem fileSaveAs;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane4;
-    private javax.swing.JScrollPane jScrollPane5;
+    private javax.swing.JTextArea inputArea;
+    private javax.swing.JScrollPane inputPanel;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuBar menuBar;
     private javax.swing.JMenuItem newBoard;
     private javax.swing.JMenu shapeMenu;
     private javax.swing.JMenu textMenu;
-    private javax.swing.JLabel userList;
+    private javax.swing.JList<String> userList;
+    private javax.swing.JLabel userListLabel;
+    private javax.swing.JScrollPane userListPanel;
     // End of variables declaration//GEN-END:variables
 }
